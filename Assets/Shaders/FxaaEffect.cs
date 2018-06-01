8f5a7af203fe7daab2fe561ae584fa0f5a38951a
Julien Garagnon <julien.garagnon@gmail.com>
2018-06-01 17:32:13 +0200

    Add FXAA effect

--- Assets/Shaders/FxaaEffect.cs
+++ Assets/Shaders/FxaaEffect.cs
@@ -1,20 +1,56 @@
-﻿using UnityEngine;
-using System;
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
 
-[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+[ExecuteInEditMode]
+[ImageEffectAllowedInSceneView]
 public class FxaaEffect : MonoBehaviour
 {
+    public enum LuminanceMode
+    {
+        Alpha,
+        Green,
+        Calculate
+    }
+
+    private const int luminancePass = 0;
+    private const int fxaaPass      = 1;
+
+    public LuminanceMode luminanceSource;
+
+    [Range(0.0312f, 0.0833f)] public float contrastThreshold = 0.0312f;
+    [Range(0.063f, 0.333f)]   public float relativeThreshold = 0.063f;
+    [Range(0f, 1f)]           public float subpixelBlending  = 1f;
+
+
     [HideInInspector] public Shader fxaaShader;
 
-    [NonSerialized] Material fxaaMaterial;
+    [NonSerialized] private Material fxaaMaterial;
 
-    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         if (fxaaMaterial == null) {
-            fxaaMaterial           = new Material(fxaaShader);
-            fxaaMaterial.hideFlags = HideFlags.HideAndDontSave;
+            fxaaMaterial = new Material(fxaaShader) {hideFlags = HideFlags.HideAndDontSave};
         }
 
-        Graphics.Blit(source, destination, fxaaMaterial);
+        fxaaMaterial.SetFloat("_ContrastThreshold", contrastThreshold);
+        fxaaMaterial.SetFloat("_RelativeThreshold", relativeThreshold);
+        fxaaMaterial.SetFloat("_SubpixelBlending", subpixelBlending);
+
+        if (luminanceSource == LuminanceMode.Calculate) {
+            fxaaMaterial.DisableKeyword("LUMINANCE_GREEN");
+            RenderTexture luminanceTex = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+            Graphics.Blit(source, luminanceTex, fxaaMaterial, luminancePass);
+            Graphics.Blit(luminanceTex, destination, fxaaMaterial, fxaaPass);
+            RenderTexture.ReleaseTemporary(luminanceTex);
+        } else {
+            if (luminanceSource == LuminanceMode.Green) {
+                fxaaMaterial.EnableKeyword("LUMINANCE_GREEN");
+            } else {
+                fxaaMaterial.DisableKeyword("LUMINANCE_GREEN");
+            }
+
+            Graphics.Blit(source, destination, fxaaMaterial, fxaaPass);
+        }
     }
 }
